0abc36ddf3ff9e863c2d85668a0d15bf9f774f50
chickfilaman51 <krishbehera10@gmail.com>
2025-01-10 22:36:02 -0500

    fixed link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => (
   <button 
     type="button" 
     className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
-    onClick={() => window.location.href = 'https://www.youtube.com/watch?v=MF3LXxGUogc&feature=youtu.be'}
+    onClick={() => window.location.href = 'https://youtu.be/Mh5CGTBQKAg'}
   >
     View the Demo
   </button>
